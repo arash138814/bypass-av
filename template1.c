71e8651ca194fb1dc6423a7aaa25fb9a187dad74
arash138814 <arashranjbar599@gmail.com>
2025-03-08 16:15:41 +0330

    Add files via upload

---
+++ template1.c
@@ -0,0 +1,12 @@
+#include "windows.h"
+
+int main()
+{
+    unsigned char shellcode[] = ""; // Your ShellCodes
+
+    void *exec = VirtualAlloc(0, sizeof shellcode, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    memcpy(exec, shellcode, sizeof shellcode);
+    ((void (*)())exec)();
+
+    return 0;
+}
