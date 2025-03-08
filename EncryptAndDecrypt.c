20d1d5216634566411f4ce770888c782e48328e1
arash138814 <arashranjbar599@gmail.com>
2025-03-08 16:32:56 +0330

    Add files via upload

---
+++ EncryptAndDecrypt.c
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+void xor (char *ShellCode, size_t ShellCode_len, char *key, size_t key_len) {
+    for (size_t i = 0; i < ShellCode_len; i++)
+    {
+        ShellCode[i] ^= key[i % key_len];
+    }
+}
+
+    int main()
+{
+    char ShellCode[] = ""; // Your ShellCode
+    char key[] = "1";
+    xor(ShellCode, strlen(ShellCode), key, strlen(key));
+    printf("Encrypted: %s\n", ShellCode);
+    xor(ShellCode, strlen(ShellCode), key, strlen(key));
+    printf("Decrypted: %s\n", ShellCode);
+    return 0;
+}
