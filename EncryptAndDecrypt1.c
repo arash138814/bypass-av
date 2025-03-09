8effc94a85dcd51002cb69db7850dc080f99d073
arash138814 <arashranjbar599@gmail.com>
2025-03-09 13:41:01 +0330

    Update EncryptAndDecrypt1.c

--- EncryptAndDecrypt1.c
+++ EncryptAndDecrypt1.c
@@ -1,14 +1,12 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
-
 void xor (char *ShellCode, size_t ShellCode_len, char *key, size_t key_len) {
     for (size_t i = 0; i < ShellCode_len; i++)
     {
         ShellCode[i] ^= key[i % key_len];
     }
 }
-
     int main()
 {
     char ShellCode[] = ""; // Your ShellCode
